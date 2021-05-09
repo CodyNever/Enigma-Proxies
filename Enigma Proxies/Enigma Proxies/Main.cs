f0a14979a1253d5fa72264a512df7e7b7d933ce8
Cody Never <za564077@gmail.com>
2021-05-09 11:40:37 +0300

    Benchmark: async get contry

--- Enigma Proxies/Enigma Proxies/Main.cs
+++ Enigma Proxies/Enigma Proxies/Main.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Enigma_Proxies
 {
@@ -7,21 +11,66 @@ namespace Enigma_Proxies
     {
         static void Main()
         {
-            var proxies = new List<ProxyBase>()
-            {
-                new ProxyBase("0.001.001", ProxyType.HTTP, ProxyAnonymity.Elite),
-                new ProxyBase("0.001.002", ProxyAnonymity.Transparent),
-                new ProxyBase("0.001.003", ProxyType.Socks5, ProxyAnonymity.Transparent),
-                new ProxyBase("0.001.004")
-            };
+            var s = new Stopwatch();
+            s.Start();
+            var proxies = GetProxies().Result;
+            s.Stop();
+            set = s.Elapsed;
+          
+            ParallelTask(proxies);
+            ForeachTask(proxies);
 
-            EnigmaProxies.CreateRotator(proxies);
+            Console.WriteLine($"Benchmark of {proxies.Count} proxies end!");
+            Console.WriteLine($"Proxies setup {set}");
+            Console.WriteLine($"Parallel {par}");
+            Console.WriteLine($"Non parallel {non}");
             
-            var p = EnigmaProxies.GetProxy();
-            
-            //Console.WriteLine(ProxyChecker.CheckProxy(new ProxyBase("132.248.196.2:8080")));
-            Console.WriteLine(ProxyChecker.CheckProxy(new ProxyBase("6.2:8080")));
             Console.ReadKey();
+        }
+
+        private static TimeSpan set;
+        private static TimeSpan par;
+        private static TimeSpan non;
+        private static void ParallelTask(List<ProxyBase> proxies)
+        {
+            var s = new Stopwatch();
+            s.Start();
+            Parallel.ForEach(proxies, proxy =>
+            {
+                Console.WriteLine($"Parallel {proxy.Working}");
+            });
+            s.Stop();
+            par = s.Elapsed;
+        }
+
+        private static void ForeachTask(List<ProxyBase> proxies)
+        {
+            var s = new Stopwatch();
+            s.Start();
+            foreach (var proxy in proxies)
+            {
+                Console.WriteLine($"foreach {proxy.Working}");
+            }
+            s.Stop();
+            non = s.Elapsed;
+        }
+
+
+        private static async Task<List<ProxyBase>> GetProxies()
+        {
+            const string path = @"C:\Users\PC\Documents\GitHub\Enigma-Proxies\Enigma Proxies\proxies.txt";
+            var proxies = new List<ProxyBase>();
+            using (var sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                
+                string line;
+                while ((line = await sr.ReadLineAsync()) != null)
+                {
+                    proxies.Add(new ProxyBase(line));
+                }
+            }
+
+            return proxies;
         }
     }
 }
